7171e772a065ec1129299c0eb9dacb174b57663e
Tausif6942 <94221843+Tausif6942@users.noreply.github.com>
2025-06-02 10:10:49 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -28,9 +28,20 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         binding=ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        displayData();
+        myDbHelper = new DBase(this);
+
+        // Example of inserting data
+        if (myDbHelper.readAllData().getCount() == 0) {
+            myDbHelper.insertAnimal("বিড়াল", "বিড়াল একটি গৃহপালিত প্রাণী। এদের শরীর নরম লোমে ঢাকা থাকে। এরা দক্ষ ইদুর শিকারী হয়ে থাকে।", R.drawable.cat);
+            myDbHelper.insertAnimal("কুকুর", "কুকুর একটি গৃহপালিত প্রাণী। এদের ধারালো দাঁত হয় এবং এরা সাধারণত জীব বার করে রাখে।", R.drawable.dog);
+            myDbHelper.insertAnimal("ঘোড়া", "ঘোড়া একটি যাত্রীবাহী প্রাণী। এরা খুব দ্রুত ছুটতে পারে ও যাত্রী বহন কাজে ব্যবহৃত হয়।", R.drawable.horse);
+            myDbHelper.insertAnimal("গরু", "গরু একটি তৃণভোজী প্রাণী। এদের দুটি সিং হয় এবং এরা আমাদের দুধ প্রদান করে।", R.drawable.cow);
+            myDbHelper.insertAnimal("খরগোশ", "খরগোশ একটি তৃণভোজী ছোটো প্রাণী। এদের লম্বা কান এবং লোমাবৃত শরীর হয়ে থাকে।", R.drawable.rabbit);
+        }
         MainAdapter adapter = new MainAdapter(list, this);
         binding.recyclerview.setAdapter(adapter);
+        displayData();
+        adapter.notifyDataSetChanged();
         LinearLayoutManager layoutManager= new LinearLayoutManager(this);
         binding.recyclerview.setLayoutManager(layoutManager);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -40,8 +51,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
     void displayData(){
-        DBase helper = new DBase(this);
-        Cursor cursor = helper.readAllData();
+        Cursor cursor = myDbHelper.readAllData();
         if(cursor.getCount()==0){
             Toast.makeText(this, "No data", Toast.LENGTH_SHORT).show();
         }
